e7e6a55e7c01e3048cf01f33defe5b9e231cfebc
Deilan <deilan@mail.ru>
2016-11-29 18:10:01 +0300

    Added Version2.0.0 demo app.

---
+++ src/NancyNetCoreDemos.Version2.0.0/Nancy/AppRootPathProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Nancy;
+
+namespace NancyNetCoreDemos.Version2._0._0.Nancy
+{
+    public class AppRootPathProvider : IRootPathProvider
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public AppRootPathProvider(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public string GetRootPath()
+        {
+            return _environment.WebRootPath;
+        }
+    }
+}
